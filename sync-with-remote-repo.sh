384b97b583c27cdab4f30bfaeee655f589b552b4
Jens Peter Secher <jpsecher@gmail.com>
2015-12-04 11:31:01 +0100

    Do not write more than necessary to server log.

--- sync-with-remote-repo.sh
+++ sync-with-remote-repo.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-echo "--> Synchonising with remote Git repository"
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-(cd $DIR && git pull)
+(cd $DIR && git pull --quiet)
